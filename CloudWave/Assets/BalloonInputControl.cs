b111218726e47af9289bf65f0a5ee9cd7831ac1d
Marc Tanenbaum <marct@marctMBP15.local>
2017-01-20 20:28:00 -0800

    Added in a little physics

---
+++ CloudWave/Assets/BalloonInputControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonInputControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis ("Lift") > 0f) {
+            Lift ();
+        }
+	}
+
+    void Lift() {
+        GetComponent<Rigidbody2D> ().AddForce (Vector2.up * 20f);
+    }
+}
