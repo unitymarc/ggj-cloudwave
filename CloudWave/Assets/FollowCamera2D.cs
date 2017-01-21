1bee9080d8898eafd0a479567ea8e7b6192188fd
Marc Tanenbaum <marct@marctMBP15.local>
2017-01-21 15:53:50 -0800

    Elementary follow rig for camera

---
+++ CloudWave/Assets/FollowCamera2D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera2D : MonoBehaviour {
+
+
+    public Transform target;
+    public float followSpeed = .9f;
+
+
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+        Vector3 v3 = transform.position;
+        Vector3 targetV3 = target.transform.position;
+        v3.x = v3.x + ((targetV3.x - v3.x)) * followSpeed;
+        transform.position = v3;
+	}
+}
