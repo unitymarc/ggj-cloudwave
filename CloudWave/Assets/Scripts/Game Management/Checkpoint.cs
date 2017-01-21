2bdc0d2603235d9ea57aabf70526993c05885fcd
Rambod Kermanizadeh <rambod@hq.unity3d.com>
2017-01-21 10:07:38 -0800

    WIP commit to fix file issues

---
+++ CloudWave/Assets/Scripts/Game Management/Checkpoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+    public static GameManager manager;
+
+    private void Start()
+    {
+        if(manager == null)
+        {
+            manager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+        }
+    } 
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.transform.tag == "Player")
+        {
+            //TODO: Send checkpoint even to game manager
+        }
+    }
+}
