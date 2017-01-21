a68b69d8f2bcb4478b2d47dcf9b07ae0efa3f930
Sidharth Varma <sidharthsinbox@gmail.com>
2017-01-21 15:08:54 -0800

    Working Main Menu with start and quit game

---
+++ CloudWave/Assets/MainMenuButtonsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonsController : MonoBehaviour {
+
+        public void NewGameBtn(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+
+    }
+
+        public void ExitGameBtn()
+        {
+        Application.Quit();
+        }
+}
