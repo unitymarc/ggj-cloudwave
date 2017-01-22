dc9232c406ee9abdc69754b2cf442dbe8b0d2fbc
Rambod Kermanizadeh <rambod@unity3d.com>
2017-01-21 19:11:27 -0800

    Added checkpoint prefabs and base checkpoint logic

---
+++ CloudWave/Assets/Scripts/GameManagement/Checkpoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+	private int checkpointId = -1;
+	private bool alreadyReached = false;
+    void Start()
+	{
+		checkpointId = GameManager.instance.RegisterCheckpoint(this);
+    } 
+
+    void OnTriggerEnter2D(Collider2D col)
+	{
+		if (!alreadyReached && col.transform.tag == "Player")
+		{
+			GameManager.instance.PlayerReachedNewCheckpoint(checkpointId);
+			alreadyReached = true;
+        }
+    }
+}
---
+++ CloudWave/Assets/Scripts/GameManagement/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	private List<Checkpoint> checkpoints = new List<Checkpoint>();
+	private int currentCheckpoint = 0;
+	public static GameManager instance;
+
+	void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+	}
+
+	public int RegisterCheckpoint(Checkpoint checkpointToReg)
+	{
+		int checkpointId = checkpoints.Count;
+		checkpoints.Add(checkpointToReg);
+		return checkpointId;
+	}
+
+	public void PlayerReachedNewCheckpoint(int checkpointId)
+	{
+		currentCheckpoint = checkpointId;
+	}
+}
