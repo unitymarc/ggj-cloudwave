3b006d6f6e0101a7e6d5811b51b756a389fdbf49
Marc Tanenbaum <marct@marctMBP15.local>
2017-01-22 04:18:26 -0800

    Tweaks to the particles in the cutscene

--- CloudWave/Assets/Scripts/GraphicNovel/AdvanceComicAnimation.cs
+++ CloudWave/Assets/Scripts/GraphicNovel/AdvanceComicAnimation.cs
@@ -11,7 +11,7 @@ public class AdvanceComicAnimation : MonoBehaviour {
     public AudioSource music;
 
     bool buttonCanShow = true;
-    public bool fading = false;
+    bool fading = false;
 
     void Update()
     {
