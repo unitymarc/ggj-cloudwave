fc9cf8f7869ba8de354a7fbd5c4805ff21af01a7
Marc Tanenbaum <marct@marctMBP15.local>
2017-01-22 13:44:25 -0800

    Added end game controller

--- CloudWave/Assets/EndGameControl.cs
+++ CloudWave/Assets/EndGameControl.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class EndGameControl : MonoBehaviour {
 
+    public AudioSource ambientSound;
     public AudioSource endSound;
     public Vector3 landingSpot;
+    public float ambientDropoff = .95f;
+    public Animator endScreenAnimation;
 
     GameObject ship;
 
@@ -18,6 +21,9 @@ public class EndGameControl : MonoBehaviour {
         {
             ship.transform.position = Vector3.Slerp (ship.transform.position, transform.position + landingSpot, Time.deltaTime * 3f);
             ship.transform.localScale = Vector3.Slerp (ship.transform.localScale, Vector3.zero, Time.deltaTime * 3f);
+
+
+            ambientSound.volume *= ambientDropoff;
         }
     }
 
@@ -36,5 +42,14 @@ public class EndGameControl : MonoBehaviour {
         ship.GetComponent<BalloonInputControl> ().enabled = false;
         ship.GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
         ship.GetComponent<Rigidbody2D> ().gravityScale = 0f;
+
+        AudioSource[] shipSounds = ship.GetComponents<AudioSource> ();
+        foreach (AudioSource source in shipSounds) {
+            source.volume = 0;
+        }
+
+        endScreenAnimation.gameObject.SetActive (true);
+        endScreenAnimation.gameObject.transform.localScale = Vector3.zero;
+        endScreenAnimation.SetTrigger ("proceed");
     }
 }
