394c229cb80839048f6b10a58efe0b91257e6aee
Marc Tanenbaum <marct@marctMBP15.local>
2017-01-22 09:16:20 -0800

    Added ship bob

---
+++ CloudWave/Assets/Scripts/ShipBob.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipBob : MonoBehaviour {
+
+    public float cycleTime = 2.5f;
+    public float cycleMagnitude = 6f;
+
+    float time;
+    float halfCycleMagnitude;
+
+    void Start()
+    {
+        time = 0f;
+        halfCycleMagnitude = cycleMagnitude * .5f;
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        Vector3 rot = transform.rotation.eulerAngles;
+        rot.z = (Mathf.Sin (time) * cycleMagnitude) - halfCycleMagnitude;
+        transform.rotation = Quaternion.Euler (rot);
+    }
+}
