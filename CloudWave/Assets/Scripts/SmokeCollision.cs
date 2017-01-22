c12b06266c1ba5a7d7d50ef4e6f86b519e53f73f
Marc Tanenbaum <marct@marctMBP15.local>
2017-01-21 20:57:58 -0800

    Smokestack particles and collisions

---
+++ CloudWave/Assets/Scripts/SmokeCollision.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class SmokeCollision : MonoBehaviour
+{
+    ParticleSystem ps;
+
+    // these lists are used to contain the particles which match
+    // the trigger conditions each frame.
+    List<ParticleSystem.Particle> inside = new List<ParticleSystem.Particle>();
+
+
+    void OnEnable()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+
+    void OnParticleTrigger()
+    {
+        // get the particles which matched the trigger conditions this frame
+        int numInside = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Inside, inside);
+
+        if (numInside > 0) {
+            Debug.Log (ps.trigger.GetCollider (0).gameObject.name);
+            Debug.Log ("Take smoke damage");
+        }
+    }
+
+
+
+
+}
+
+
