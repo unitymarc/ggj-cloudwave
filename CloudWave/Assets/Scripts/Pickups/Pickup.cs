f8547d21dd52789f24219ef0ce682cffcc3a8f78
Rambod Kermanizadeh <rambod@unity3d.com>
2017-01-21 22:25:13 -0800

    Beginning of pick up work

---
+++ CloudWave/Assets/Scripts/Pickups/Pickup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+	[SerializeField]
+	protected int amount = 1;
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.transform.tag == "Player")
+		{
+			Consume(col);
+		}
+	}
+
+	protected virtual void Consume(Collider2D col)
+	{
+
+	}
+}
