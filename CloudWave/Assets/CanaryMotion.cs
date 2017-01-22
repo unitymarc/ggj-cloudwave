4536839edba4a25a5e49d56e797cdd966dc68c46
Marc Tanenbaum <marct@marctMBP15.local>
2017-01-22 11:53:32 -0800

    Canary lives

---
+++ CloudWave/Assets/CanaryMotion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanaryMotion : MonoBehaviour {
+    public Transform follow;
+    public Vector3 offset;
+    public AudioSource audio;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+        Vector2 v2 = Vector2.Lerp (transform.position, follow.position + offset, Time.deltaTime);
+        transform.position = v2;
+	}
+
+    public void Kill()
+    {
+        audio.Play ();
+        GetComponent<Rigidbody2D> ().gravityScale = 1f;
+    }
+}
