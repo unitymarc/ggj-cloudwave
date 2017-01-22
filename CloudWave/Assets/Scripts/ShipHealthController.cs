bc1f7c92df55c365630c1fc32b70755c084356f7
Rambod Kermanizadeh <rambod@unity3d.com>
2017-01-22 12:45:35 -0800

    Canaries spawn and get removed

--- CloudWave/Assets/Scripts/ShipHealthController.cs
+++ CloudWave/Assets/Scripts/ShipHealthController.cs
@@ -6,9 +6,36 @@ using UnityEngine;
 public class ShipHealthController : MonoBehaviour {
 	[SerializeField]
 	private int numberOfCanaries = 3;
+	public GameObject canaryPrefab;
+	private List<CanaryMotion> canaries = new List<CanaryMotion>();
+
+	void Awake()
+	{
+		for (int i = 0; i < numberOfCanaries; i++)
+		{
+			SpawnCanary();
+		}
+	}
+
+	private void SpawnCanary()
+	{
+		Vector3 offset = new Vector3(UnityEngine.Random.Range(-6f, -4f), UnityEngine.Random.Range(-2f, 2f));
+		var canary = (Instantiate(canaryPrefab, offset, Quaternion.identity) as GameObject).GetComponent<CanaryMotion>();
+		canary.follow = transform;
+		canary.offset = offset;
+		canaries.Add(canary);
+	}
+
+	private void KillCanary()
+	{
+		var canaryToKill = canaries[canaries.Count - 1];
+		canaries.Remove(canaryToKill);
+		canaryToKill.Kill();
+	}
 
 	public void RemoveCanary () {
 		numberOfCanaries--;
+		KillCanary();
 		if(numberOfCanaries <= 0) {
 			InstaKill();
 		}
@@ -26,15 +53,27 @@ public class ShipHealthController : MonoBehaviour {
 
 	public void AddCanary () {
 		numberOfCanaries++;
+		SpawnCanary();
 	}
 
 	public void AddCanary(int amount)
 	{
 		numberOfCanaries += amount;
+		for (int i = 0; i < amount; i++)
+		{
+			SpawnCanary();
+		}
 	}
 
 	public void InstaKill () {
 		GameManager.instance.PlayerDied();
+		if (numberOfCanaries > 0)
+		{
+			for (int i = 0; i < canaries.Count; i++)
+			{
+				KillCanary();
+			}
+		}
 		//GameObject.Destroy(gameObject);
 	}
 }
